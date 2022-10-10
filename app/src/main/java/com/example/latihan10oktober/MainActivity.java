29df567c290371be8f819e6d894e3417fc0034a5
Asteveee <114042520+Asteveee@users.noreply.github.com>
2022-10-10 11:19:04 +0700

    new

--- app/src/main/java/com/example/latihan10oktober/MainActivity.java
+++ app/src/main/java/com/example/latihan10oktober/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.latihan10oktober;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnButton =(Button)findViewById(R.id.bt_Button);
+
+        btnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Apa Kabar Kamu", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
