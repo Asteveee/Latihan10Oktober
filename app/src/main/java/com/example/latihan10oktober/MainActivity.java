a09ac267080123d49487b64754559c71c57566df
Asteveee <114042520+Asteveee@users.noreply.github.com>
2022-10-10 10:48:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihan10oktober/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.latihan10oktober;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
